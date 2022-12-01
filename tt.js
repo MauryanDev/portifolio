0f30c5fbe2437960476450e87e7bbfd3458baa5e
MauryanDev <118943761+MauryanDev@users.noreply.github.com>
2022-12-01 20:04:37 -0300

    Add files via upload

--- tt.js
+++ tt.js
@@ -1,6 +1,19 @@
 
 function enviarr() {
-    bebidas = document.getElementById("Nbebidas").value;
+    consumido = document.getElementById("consumido").value;
     peso = document.getElementById("pesoo").value;
-    document.getElementById("saida").innerHTML ="tudo"+ peso + bebidas
+    teoralc= document.querySelector('input[name="teor alcoolico"]:checked').value;
+    
+    alcool = ((consumido * teoralc * 0.8) / (peso * 0.6));
+
+    document.getElementById("teor").value = alcool.toFixed(2);
+
+
+    if (alcool >= 0.3) {
+        document.getElementById("dirigir").value = "Inapto"
+
+    } else {
+        document.getElementById("dirigir").value = "Apto"
+    }
+
 }
