8df4a9035631d08f8ecfc15d2549ce7e9f7fc6da
MauryanDev <118943761+MauryanDev@users.noreply.github.com>
2023-05-10 09:51:06 -0300

    Add files via upload

--- Projetos/agenda/agenda.js
+++ Projetos/agenda/agenda.js
@@ -18,8 +18,12 @@ var emailsVet = []
 const contatosContainer = document.querySelector(".contatos");
 const validateInput = () => nome.value.trim().length > 0;
 
+const validateNumero = () =>tiraHifen(inteirosPositivos(numero.value)).trim().length === 11;
+
+
 const handleAddTask = () => {
     const inputIsValid = validateInput();
+    const validNumero = validateNumero();
 
     if (!inputIsValid) {
         nome.onblur = function () {
@@ -31,9 +35,37 @@ const handleAddTask = () => {
             nome.placeholder = "Nome";
             nome.classList.remove("error");
         };
-
         return
     }
+    if(!validNumero){
+        numero.value= ''
+        numero.onblur =  function () {
+            numero.placeholder = "Deve conter 11 dígitos";
+            numero.classList.add("error");
+        };
+        numero.onblur()
+        numero.onfocus = function () {
+            numero.placeholder = "Ex:(DDD)Numero";
+            numero.classList.remove("error");
+        };
+        return
+    }
+    if(!validateEmail(email.value) && email.value.length > 0){
+        email.value=''
+        email.onblur= function () {
+            email.placeholder = "Email invalido";
+            email.classList.add("error");
+
+        };
+        email.onblur()
+
+        email.onfocus = function () {
+            email.placeholder = "Ex:silva@gmail.com";
+            email.classList.remove("error");
+        };
+        return
+    }
+
     if (localStorage.nomes && test == 1) {
         contatosContainer.innerHTML = ''
         renov()
@@ -48,11 +80,12 @@ const handleAddTask = () => {
     img.classList.add("ph-duotone");
     img.classList.add("ph-user-circle");
     img.style = "font-size: 80px;"
+    img.addEventListener('click', () => editar(nomes))
 
     const nomes = document.createElement("h1")
     const numeros = document.createElement("p")
     nomes.innerText = nome.value;
-    numeros.innerText = numero.value;
+    numeros.innerText = mask(numero.value);
     nomes.addEventListener('click', () => editar(nomes))
 
 
@@ -82,13 +115,16 @@ function handleBack() {
 function renov() {
     nomesVet = JSON.parse(localStorage.getItem('nomes'));
     numerosVet = JSON.parse(localStorage.getItem('numeros'));
+    emailsVet = JSON.parse(localStorage.getItem('emails'))
     let indext = id;
     if (test == 1) {
         nomesVet.splice(indext, 1, nome.value);
         numerosVet.splice(indext, 1, numero.value);
+        emailsVet.splice(indext, 1, email.value);
 
         localStorage.nomes = JSON.stringify(nomesVet);
         localStorage.numeros = JSON.stringify(numerosVet);
+        localStorage.emails = JSON.stringify(emailsVet);
     }
 }
 
@@ -102,10 +138,16 @@ function editar(elemento) {
     const pai = elemento.parentNode;
     const array = pai.children;
     const h1 = array[0].innerText;
-    document.getElementById('nomec').innerHTML= array[0].innerText;
+    nomesVet = JSON.parse(localStorage.getItem('nomes'))
+    emailsVet = JSON.parse(localStorage.getItem('emails'))
+
+    const em = nomesVet.indexOf(h1)
+
+    document.getElementById('nomec').innerHTML = array[0].innerText;
     document.querySelector(".nomeinput").value = array[0].innerText;
     document.querySelector(".numeroinput").value = array[1].innerText;
-    
+
+    document.querySelector(".emailinput").value = emailsVet[em];
     mostraind(h1);
     abrir();
     test = 1;
@@ -128,21 +170,32 @@ function fechar() {
     nome.onblur = function () {
         nome.placeholder = "Nome";
     };
-    nome.onblur()
+    numero.onblur = function () {
+        numero.placeholder = "Ex:(DDD)Numero";
+    };
+    email.onblur = function () {
+        email.placeholder = "Ex:silva@gmail.com";
+    };
+    nome.onblur();
+    numero.onblur();
+    email.onblur();
     nome.classList.remove("error");
+    numero.classList.remove("error");
+    email.classList.remove('error')
     document.getElementById('popup').style.display = 'none';
     document.getElementById('tudo').style.display = 'block';
     document.querySelector(".nomeinput").value = '';
     document.querySelector(".numeroinput").value = '';
     document.querySelector(".emailinput").value = '';
-    document.getElementById('nomec').innerHTML= '';
+    document.getElementById('nomec').innerHTML = '';
 }
 
 
 function updateLocalstorage() {
     if (localStorage.nomes) {
-        nomesVet = JSON.parse(localStorage.getItem('nomes'));;
+        nomesVet = JSON.parse(localStorage.getItem('nomes'));
         numerosVet = JSON.parse(localStorage.getItem('numeros'));
+        emailsVet = JSON.parse(localStorage.getItem('emails'));
     }
     const inputIsValid = validateInput();
 
@@ -150,10 +203,12 @@ function updateLocalstorage() {
         return
     }
     nomesVet.push(nome.value);
-    numerosVet.push(numero.value);
+    numerosVet.push(mask(numero.value));
+    emailsVet.push(email.value);
     mostracont();
     localStorage.nomes = JSON.stringify(nomesVet);
     localStorage.numeros = JSON.stringify(numerosVet);
+    localStorage.emails = JSON.stringify(emailsVet);
 }
 
 function showValues(i) {
@@ -170,6 +225,7 @@ function showValues(i) {
         img.classList.add("ph-duotone");
         img.classList.add("ph-user-circle");
         img.style = "font-size: 80px;"
+        img.addEventListener('click', () => editar(nomes))
 
         const nomes = document.createElement("h1")
         const numeros = document.createElement("p")
@@ -200,20 +256,23 @@ function removeSave(elemento) {
     const pai = elemento.parentNode;
     const array = pai.children;
     const span = array[1];
-    const spanarray =span.children;
-    const h1=spanarray[0].innerText;
+    const spanarray = span.children;
+    const h1 = spanarray[0].innerText;
 
     nomesVet = JSON.parse(localStorage.getItem('nomes'));
     numerosVet = JSON.parse(localStorage.getItem('numeros'));
+    emailsVet = JSON.parse(localStorage.getItem('emails'))
 
     const index = nomesVet.indexOf(h1);
     if (index > -1) {
         nomesVet.splice(index, 1);
         numerosVet.splice(index, 1);
+        emailsVet.splice(index, 1)
     }
     mostracont()
     localStorage.nomes = JSON.stringify(nomesVet);
     localStorage.numeros = JSON.stringify(numerosVet);
+    localStorage.emails = JSON.stringify(emailsVet);
 }
 showValues()
 /*pesquisa */
@@ -236,6 +295,7 @@ pesquisar.oninput = () => {
         img.classList.add("ph-duotone");
         img.classList.add("ph-user-circle");
         img.style = "font-size: 80px;"
+        img.addEventListener('click', () => editar(nomes))
 
         const nomes = document.createElement("h1")
         const numeros = document.createElement("p")
@@ -267,4 +327,27 @@ mostracont();
 function mostracont() {
     contador.innerHTML = nomesVet.length;
 }
+/*restrições*/
+function mask(numero) {
+    let numeroAtual = numero
+    const parte0 = numeroAtual.slice(0,2);
+    const parte1 = numeroAtual.slice(2,7);
+    const parte2 = numeroAtual.slice(7,11);
+    numeroAjustado = `(${parte0})${parte1}-${parte2}`
+        return numeroAjustado
+}
+function tiraHifen(numero) {
+    const numeroAtual = numero;
+    const numeroAjustado0 = numeroAtual.replace(/\-/g, '');
+    const numeroAjustado1 = numeroAjustado0.replace(/[{()}]/g, '');
+   return  numeroAjustado1
+}
+function inteirosPositivos(numero) {
+    numero = numero.replace(/[^0-9]/g, "") 
+    return numero;
+} 
 
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
