353a7bd73e70b445d9cd32674324ede15238c9ec
MauryanDev <118943761+MauryanDev@users.noreply.github.com>
2023-05-11 23:45:01 -0300

    Add files via upload

---
+++ Projetos/agenda/dados.js
@@ -0,0 +1,119 @@
+var vtddd = [
+    {
+        nome: 'DistritoFederal',
+        numero: ['61'],
+    },
+    {
+        nome: 'Goiás',
+        numero: ['62', '64'],
+    },
+    {
+        nome: 'Mato Grosso',
+        numero: ['65', '66'],
+    },
+    {
+        nome: 'Mato Grosso do Sul',
+        numero: ['67'],
+    },
+    {
+        nome: ' Alagoas',
+        numero: ['82'],
+    },
+    {
+        nome: 'Bahia',
+        numero: ['71', '73', '74', '75', '77'],
+    },
+    {
+        nome: 'Ceará',
+        numero: ['85', '88'],
+    },
+    {
+        nome: 'Maranhão',
+        numero: ['98', '99']
+    },
+    {
+        nome: 'Paraíba',
+        numero: ['83'],
+    },
+    {
+        nome: 'Pernambuco',
+        numero: ['81', '87'],
+    },
+    {
+        nome: 'Ceará',
+        numero: ['85', '88'],
+    },
+    {
+        nome: 'Rio Grande do Norte',
+        numero: ['84'],
+    },
+    {
+        nome: 'Sergipe',
+        numero: ['79'],
+    },
+    {
+        nome: 'Acre',
+        numero: ['68'],
+    },
+    {
+        nome: 'Amapá',
+        numero: ['96'],
+    },
+    {
+        nome: 'Amazonas',
+        numero: ['92', '97'],
+    },
+    {
+        nome: 'Pará',
+        numero: ['91', '93', '94'],
+    },
+    {
+        nome: 'Rondônia',
+        numero: ['69'],
+    },
+    {
+        nome: 'Roraima',
+        numero: ['95'],
+    },
+    {
+        nome: 'Tocantins',
+        numero: ['63'],
+    },
+    {
+        nome: 'Espírito Santo',
+        numero: ['27', '28'],
+    },
+    {
+        nome: 'Minas Gerais',
+        numero: ['31', '32', '33', '34', '35', '37', '38'],
+    },
+    {
+        nome: 'Rio de Janeiro',
+        numero: ['21', '22', '24'],
+    },
+    {
+        nome: 'São Paulo',
+        numero: ['11', '12', '13', '14', '15', '16', '17', '18', '19'],
+    },
+    {
+        nome: 'Paraná',
+        numero: ['41', '42', '43', '44', '45', '46'],
+    },
+    {
+        nome: 'Rio Grande do Sul',
+        numero: ['51', '53', '54', '55'],
+    },
+    {
+        nome: 'Santa Catarina',
+        numero: ['47', '48', '49'],
+    },
+    {
+        nome: 'Piauí',
+        numero: ['86', '89'],
+    },
+    {
+        nome: 'Minas Gerais ',
+        numero: ['31', '32', '33', '34', '35', '37', '38'],
+    },
+]
+
